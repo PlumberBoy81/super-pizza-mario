1213da9c4f5822c265473174e1e8bf8d4cc63496
PlumberBoy81 <106181702+PlumberBoy81@users.noreply.github.com>
2025-05-30 09:32:23 -0400

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -159,18 +159,6 @@ let flierIdle : animation.Animation = null
 let flierFlying : animation.Animation = null
 let mainIdleRight : animation.Animation = null
 let mainIdleLeft : animation.Animation = null
-let luigiIdleLeft : animation.Animation = null
-let luigiIdleRight : animation.Animation = null
-let luigiJumpLeft : animation.Animation = null
-let luigiJumpRight : animation.Animation = null
-let luigiRunLeft : animation.Animation = null
-let luigiRunRight : animation.Animation = null
-let warioIdleLeft : animation.Animation = null
-let warioIdleRight : animation.Animation = null
-let warioJumpLeft : animation.Animation = null
-let warioJumpRight : animation.Animation = null
-let warioRunLeft : animation.Animation = null
-let warioRunRight : animation.Animation = null
 let doubleJumpSpeed = 0
 let canDoubleJump = false
 let coinAnimation : animation.Animation = null
@@ -182,14 +170,11 @@ let invincibilityPeriod = 0
 let hero : Sprite = null
 let mainCrouchRight = null
 let mainCrouchLeft = null
+hero = sprites.create(assets.image`Mario_IdleRight`, SpriteKind.Player)
 invincibilityPeriod = 600
 //  how long to pause between each contact with a single enemy
 pixelsToMeters = 30
 gravity = 9.81 * pixelsToMeters
-let currentPlayer = "Mario"
-//  Track current character
-hero = sprites.create(assets.image`Mario_IdleRight`, SpriteKind.Player)
-//  Initialize as Mario
 scene.setBackgroundColor(9)
 initializeAnimations()
 createPlayer(hero)
@@ -201,91 +186,49 @@ function initializeAnimations() {
     initializeHeroAnimations()
     initializeCoinAnimation()
     initializeFlierAnimations()
-    initializeLuigiAnimations()
-    //  Initialize Luigi animations
-    initializeWarioAnimations()
 }
 
-//  Initialize Wario animations
 function initializeCoinAnimation() {
     
     coinAnimation = animation.createAnimation(ActionKind.Idle, 200)
     coinAnimation.addAnimationFrame(assets.image`
-         Coin
-         `)
+        Coin
+        `)
 }
 
 function initializeHeroAnimations() {
     animateRun()
-    //  Initialize Mario's run animations
     animateIdle()
-    //  Initialize Mario's idle animations
     animateJumps()
-}
-
-//  Initialize Mario's jump animations
-function initializeLuigiAnimations() {
-    
-    luigiIdleLeft = animation.createAnimation(ActionKind.IdleLeft, 100)
-    luigiIdleLeft.addAnimationFrame(assets.image`Luigi_IdleLeft`)
-    luigiIdleRight = animation.createAnimation(ActionKind.IdleRight, 100)
-    luigiIdleRight.addAnimationFrame(assets.image`Luigi_IdleRight`)
-    luigiJumpLeft = animation.createAnimation(ActionKind.JumpingLeft, 100)
-    luigiJumpLeft.addAnimationFrame(assets.image`Luigi_JumpLeft`)
-    luigiJumpRight = animation.createAnimation(ActionKind.JumpingRight, 100)
-    luigiJumpRight.addAnimationFrame(assets.image`Luigi_JumpRight`)
-    luigiRunLeft = animation.createAnimation(ActionKind.RunningLeft, 200)
-    animation.attachAnimation(hero, luigiRunLeft)
-    luigiRunLeft.addAnimationFrame(assets.image`Luigi_RunLeft0`)
-    luigiRunLeft.addAnimationFrame(assets.image`Luigi_RunLeft1`)
-    luigiRunLeft.addAnimationFrame(assets.image`Luigi_RunLeft2`)
-    luigiRunRight = animation.createAnimation(ActionKind.RunningRight, 200)
-    animation.attachAnimation(hero, luigiRunRight)
-    luigiRunRight.addAnimationFrame(assets.image`Luigi_RunRight0`)
-    luigiRunRight.addAnimationFrame(assets.image`Luigi_RunRight1`)
-    luigiRunRight.addAnimationFrame(assets.image`Luigi_RunRight2`)
-}
-
-//  Initialize Wario animations
-function initializeWarioAnimations() {
-    
-    warioIdleLeft = animation.createAnimation(ActionKind.IdleLeft, 100)
-    warioIdleLeft.addAnimationFrame(assets.image`Wario_IdleLeft`)
-    warioIdleRight = animation.createAnimation(ActionKind.IdleRight, 100)
-    warioIdleRight.addAnimationFrame(assets.image`Wario_IdleRight`)
-    warioJumpLeft = animation.createAnimation(ActionKind.JumpingLeft, 100)
-    warioJumpLeft.addAnimationFrame(assets.image`Wario_JumpLeft`)
-    warioJumpRight = animation.createAnimation(ActionKind.JumpingRight, 100)
-    warioJumpRight.addAnimationFrame(assets.image`Wario_JumpRight`)
-    warioRunLeft = animation.createAnimation(ActionKind.RunningLeft, 200)
-    animation.attachAnimation(hero, warioRunLeft)
-    warioRunLeft.addAnimationFrame(assets.image`Wario_RunLeft0`)
-    warioRunLeft.addAnimationFrame(assets.image`Wario_RunLeft1`)
-    warioRunLeft.addAnimationFrame(assets.image`Wario_RunLeft2`)
-    warioRunRight = animation.createAnimation(ActionKind.RunningRight, 200)
-    animation.attachAnimation(hero, warioRunRight)
-    warioRunRight.addAnimationFrame(assets.image`Wario_RunRight0`)
-    warioRunRight.addAnimationFrame(assets.image`Wario_RunRight1`)
-    warioRunRight.addAnimationFrame(assets.image`Wario_RunRight2`)
 }
 
 function initializeFlierAnimations() {
     
     flierFlying = animation.createAnimation(ActionKind.Flying, 700)
-    flierFlying.addAnimationFrame(assets.image`squid`)
-    flierFlying.addAnimationFrame(assets.image`squid_fly`)
+    flierFlying.addAnimationFrame(assets.image`
+        squid
+        `)
+    flierFlying.addAnimationFrame(assets.image`
+        squid_fly
+        `)
     flierIdle = animation.createAnimation(ActionKind.Idle, 100)
-    flierIdle.addAnimationFrame(assets.image`squid`)
+    flierIdle.addAnimationFrame(assets.image`
+        squid
+        `)
 }
 
 function animateIdle() {
     
     mainIdleLeft = animation.createAnimation(ActionKind.IdleLeft, 100)
     animation.attachAnimation(hero, mainIdleLeft)
-    mainIdleLeft.addAnimationFrame(assets.image`Mario_IdleLeft`)
+    mainIdleLeft.addAnimationFrame(assets.image`
+        Mario_IdleLeft
+        `)
     mainIdleRight = animation.createAnimation(ActionKind.IdleRight, 100)
     animation.attachAnimation(hero, mainIdleRight)
-    mainIdleRight.addAnimationFrame(assets.image`Mario_IdleRight`)
+    mainIdleRight.addAnimationFrame(assets.image`
+        Mario_IdleRight
+        `)
 }
 
 function animateRun() {
@@ -297,25 +240,23 @@ function animateRun() {
     mainRunLeft.addAnimationFrame(assets.image`Mario_RunLeft2`)
     mainRunRight = animation.createAnimation(ActionKind.RunningRight, 200)
     animation.attachAnimation(hero, mainRunRight)
-    mainRunRight.addAnimationFrame(assets.image`
-        Mario_RunRight0
-        `)
-    mainRunRight.addAnimationFrame(assets.image`
-         Mario_RunRight1
-         `)
-    mainRunRight.addAnimationFrame(assets.image`
-         Mario_RunRight2
-         `)
+    mainRunRight.addAnimationFrame(assets.image`Mario_RunRight0`)
+    mainRunRight.addAnimationFrame(assets.image`Mario_RunRight1`)
+    mainRunRight.addAnimationFrame(assets.image`Mario_RunRight2`)
 }
 
 function animateJumps() {
     
     mainJumpLeft = animation.createAnimation(ActionKind.JumpingLeft, 100)
     animation.attachAnimation(hero, mainJumpLeft)
-    mainJumpLeft.addAnimationFrame(assets.image`Mario_JumpLeft`)
+    mainJumpLeft.addAnimationFrame(assets.image`
+        Mario_JumpLeft
+        `)
     mainJumpRight = animation.createAnimation(ActionKind.JumpingRight, 100)
     animation.attachAnimation(hero, mainJumpRight)
-    mainJumpRight.addAnimationFrame(assets.image`Mario_JumpRight`)
+    mainJumpRight.addAnimationFrame(assets.image`
+        Mario_JumpRight
+        `)
 }
 
 // ### Collisions ####
@@ -341,7 +282,7 @@ function overlap_enemy(sprite: Sprite, otherSprite: Sprite) {
         music.powerUp.play()
     } else {
         info.changeLifeBy(-1)
-        sprite.say("Ouch!", invincibilityPeriod)
+        sprite.say("Mamma mia!", invincibilityPeriod)
         music.powerDown.play()
         pause(invincibilityPeriod)
     }
@@ -370,92 +311,16 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function on_down_pressed(
     }
     
 })
-//  Character Switching
-controller.B.onEvent(ControllerButtonEvent.Pressed, function on_b_pressed() {
-    switchCharacter()
-})
-function switchCharacter() {
-    
-    let current_x = hero.x
-    let current_y = hero.y
-    //  Save the current score and life count
-    let current_score = info.score()
-    let current_life = info.life()
-    if (currentPlayer == "Mario") {
-        hero.destroy()
-        //  Remove the current sprite
-        hero = sprites.create(assets.image`Luigi_IdleRight`, SpriteKind.Player)
-        //  Switch to Luigi
-        attachLuigiAnimations()
-        currentPlayer = "Luigi"
-        createPlayer(hero)
-    } else if (currentPlayer == "Luigi") {
-        hero.destroy()
-        //  Remove the current sprite
-        hero = sprites.create(assets.image`Wario_IdleRight`, SpriteKind.Player)
-        //  Switch to Wario
-        attachWarioAnimations()
-        currentPlayer = "Wario"
-        createPlayer(hero)
-    } else {
-        //  Switch back to Mario
-        hero.destroy()
-        //  Remove the current sprite
-        hero = sprites.create(assets.image`Mario_IdleRight`, SpriteKind.Player)
-        //  Switch to Mario
-        attachMarioAnimations()
-        currentPlayer = "Mario"
-        createPlayer(hero)
-    }
-    
-    //  Set the new player's position to the previous position
-    hero.setPosition(current_x, current_y)
-    //  Restore the score and life count to the saved values
-    info.setScore(current_score)
-    info.setLife(current_life)
-}
-
-function attachLuigiAnimations() {
-    animation.attachAnimation(hero, luigiIdleLeft)
-    //  Attach Luigi animations
-    animation.attachAnimation(hero, luigiIdleRight)
-    animation.attachAnimation(hero, luigiJumpLeft)
-    animation.attachAnimation(hero, luigiJumpRight)
-    animation.attachAnimation(hero, luigiRunLeft)
-    //  Attach Luigi run animations
-    animation.attachAnimation(hero, luigiRunRight)
-}
-
-function attachWarioAnimations() {
-    animation.attachAnimation(hero, warioIdleLeft)
-    //  Attach Wario animations
-    animation.attachAnimation(hero, warioIdleRight)
-    animation.attachAnimation(hero, warioJumpLeft)
-    animation.attachAnimation(hero, warioJumpRight)
-    animation.attachAnimation(hero, warioRunLeft)
-    animation.attachAnimation(hero, warioRunRight)
-}
-
-function attachMarioAnimations() {
-    animation.attachAnimation(hero, mainIdleLeft)
-    //  Attach Mario animations
-    animation.attachAnimation(hero, mainIdleRight)
-    animation.attachAnimation(hero, mainJumpLeft)
-    animation.attachAnimation(hero, mainJumpRight)
-    animation.attachAnimation(hero, mainRunLeft)
-    //  Attach Mario run animations
-    animation.attachAnimation(hero, mainRunRight)
-}
-
 function attemptJump() {
     
-    let jumpStrength = currentPlayer == "Mario" ? -4.25 * pixelsToMeters : (currentPlayer == "Luigi" ? -4.8 * pixelsToMeters : -3.75 * pixelsToMeters)
+    //  else if: either fell off a ledge, or double jumping
     if (hero.isHittingTile(CollisionDirection.Bottom)) {
-        hero.vy = jumpStrength
+        hero.vy = -4 * pixelsToMeters
     } else if (canDoubleJump) {
-        doubleJumpSpeed = -4.25 * pixelsToMeters
+        doubleJumpSpeed = -5 * pixelsToMeters
+        //  Good double jump
         if (hero.vy >= -60) {
-            doubleJumpSpeed = -4.25 * pixelsToMeters
+            doubleJumpSpeed = -4 * pixelsToMeters
             hero.startEffect(effects.trail, 500)
             scene.cameraShake(2, 250)
         }
@@ -470,13 +335,21 @@ function attemptJump() {
 function setLevelTileMap(level: number) {
     clearGame()
     if (level == 0) {
-        tiles.setTilemap(tilemap`level_0`)
+        tiles.setTilemap(tilemap`
+            level_0
+            `)
     } else if (level == 1) {
-        tiles.setTilemap(tilemap`level_1`)
+        tiles.setTilemap(tilemap`
+            level_1
+            `)
     } else if (level == 2) {
-        tiles.setTilemap(tilemap`level_2`)
+        tiles.setTilemap(tilemap`
+            level_2
+            `)
     } else if (level == 3) {
-        tiles.setTilemap(tilemap`level_3`)
+        tiles.setTilemap(tilemap`
+            level_3
+            `)
     } else if (level == 4) {
         
     }
@@ -501,15 +374,21 @@ function clearGame() {
 
 function createEnemies() {
     
+    //  enemy that moves back and forth
     for (let value5 of tiles.getTilesByType(myTiles.tile4)) {
-        bumper = sprites.create(assets.image`goomba`, SpriteKind.Bumper)
+        bumper = sprites.create(assets.image`
+            goomba
+            `, SpriteKind.Bumper)
         tiles.placeOnTile(bumper, value5)
         tiles.setTileAt(value5, myTiles.tile0)
         bumper.ay = gravity
         bumper.vx = 30
     }
+    //  enemy that flies at player
     for (let value6 of tiles.getTilesByType(myTiles.tile7)) {
-        flier = sprites.create(assets.image`squid`, SpriteKind.Flier)
+        flier = sprites.create(assets.image`
+            squid
+            `, SpriteKind.Flier)
         tiles.placeOnTile(flier, value6)
         tiles.setTileAt(value6, myTiles.tile0)
         animation.attachAnimation(flier, flierFlying)
@@ -552,6 +431,7 @@ function spawnGoals() {
 }
 
 // ### Update ####
+//  bumper movement
 game.onUpdate(function on_update() {
     for (let value9 of sprites.allOfKind(SpriteKind.Bumper)) {
         if (value9.isHittingTile(CollisionDirection.Left)) {
@@ -562,6 +442,7 @@ game.onUpdate(function on_update() {
         
     }
 })
+//  Flier movement
 game.onUpdate(function on_update2() {
     for (let value8 of sprites.allOfKind(SpriteKind.Flier)) {
         if (Math.abs(value8.x - hero.x) < 60) {
@@ -594,7 +475,7 @@ game.onUpdate(function on_update3() {
     }
     
 })
-//  Set up hero animations
+//  set up hero animations
 game.onUpdate(function on_update4() {
     
     if (hero.vx < 0) {
@@ -608,58 +489,18 @@ game.onUpdate(function on_update4() {
     }
     
     if (hero.vy < 20 && !hero.isHittingTile(CollisionDirection.Bottom)) {
-        if (currentPlayer == "Luigi") {
-            if (heroFacingLeft) {
-                animation.setAction(hero, ActionKind.JumpingLeft)
-            } else {
-                //  Luigi jump animation
-                animation.setAction(hero, ActionKind.JumpingRight)
-            }
-            
-        } else if (currentPlayer == "Wario") {
-            //  Luigi jump animation
-            if (heroFacingLeft) {
-                animation.setAction(hero, ActionKind.JumpingLeft)
-            } else {
-                //  Wario jump animation
-                animation.setAction(hero, ActionKind.JumpingRight)
-            }
-            
-        } else if (heroFacingLeft) {
+        if (heroFacingLeft) {
             animation.setAction(hero, ActionKind.JumpingLeft)
         } else {
             animation.setAction(hero, ActionKind.JumpingRight)
         }
         
     } else if (hero.vx < 0) {
-        if (currentPlayer == "Luigi") {
-            animation.setAction(hero, ActionKind.RunningLeft)
-        } else if (currentPlayer == "Wario") {
-            animation.setAction(hero, ActionKind.RunningLeft)
-        } else {
-            //  Wario runs
-            animation.setAction(hero, ActionKind.RunningLeft)
-        }
-        
+        animation.setAction(hero, ActionKind.RunningLeft)
     } else if (hero.vx > 0) {
-        if (currentPlayer == "Luigi") {
-            animation.setAction(hero, ActionKind.RunningRight)
-        } else if (currentPlayer == "Wario") {
-            animation.setAction(hero, ActionKind.RunningRight)
-        } else {
-            //  Wario runs
-            animation.setAction(hero, ActionKind.RunningRight)
-        }
-        
+        animation.setAction(hero, ActionKind.RunningRight)
     } else if (heroFacingLeft) {
-        if (currentPlayer == "Luigi") {
-            animation.setAction(hero, ActionKind.IdleLeft)
-        } else {
-            animation.setAction(hero, ActionKind.IdleLeft)
-        }
-        
-    } else if (currentPlayer == "Luigi") {
-        animation.setAction(hero, ActionKind.IdleRight)
+        animation.setAction(hero, ActionKind.IdleLeft)
     } else {
         animation.setAction(hero, ActionKind.IdleRight)
     }
